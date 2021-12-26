80173e5ce902d83634287e8f32108e622a9a22b1
Maxmudov13 <maxmudovilyos2003@gmail.com>
2021-12-27 04:00:30 +0500

    Initial Commit

---
+++ NewLoginPage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewLoginPage
+//
+//  Created by user on 27/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewLoginPage/NewLoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewLoginPageApp.swift
+//  NewLoginPage
+//
+//  Created by user on 27/12/21.
+//
+
+import SwiftUI
+
+@main
+struct NewLoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
